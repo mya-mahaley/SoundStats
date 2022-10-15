32a63080d1edec43acdcd8569234741007873694
mya-mahaley <mya.mahaley@utexas.edu>
2022-10-15 18:34:04 -0500

    Basic Project Creation

---
+++ SoundStats/SoundStats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoundStats
+//
+//  Created by Mya Mahaley on 10/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
